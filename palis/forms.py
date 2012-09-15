8d30face482f590dcfb73ea8bddb6f52f4713826
czh <chsc4698@gmail.com>
2012-09-15 16:20:31 +0800

    trivial modifications

--- palis/forms.py
+++ palis/forms.py
@@ -5,6 +5,6 @@ class LoginForm(Form, object):
     username = TextField(u'Username', validators=[validators.Required()])
     password = PasswordField(u'Password', validators=[validators.Required()])
 
-    def __init__(self, **kwargs):
-        super(LoginForm, self).__init__(**kwargs)
+    def __init__(self, form, **kwargs):
+        super(LoginForm, self).__init__(form, **kwargs)
 
