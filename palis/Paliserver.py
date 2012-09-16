d2f6698d8f107a32896d6c313f242e8f10e0e76c
czh <chsc4698@gmail.com>
2012-09-17 01:59:39 +0800

    forward dialog introduced

--- palis/Paliserver.py
+++ palis/Paliserver.py
@@ -7,19 +7,31 @@ from flask import session, request, redirect, url_for, flash
 from flask.templating import render_template
 import wtforms
 from palis import app, db
-from palis.forms import LoginForm
+from palis.forms import LoginForm, ForwardForm
 from palis.models import User, PaperDispatchEntity
 
 
 state = {'agent': ''}
 
+
+@app.before_first_request
+def init_db(exception=None):
+    db.create_all()
+
+    app.jinja_env.globals.update(user_list=User.query.all())
+
+
+@app.before_request
+def init_current_user(exception=None):
+    username = session.get('username', None)
+    if username:
+        user = User.query.filter_by(username=username).first()
+
+        app.jinja_env.globals.update(cur_uid=user._id, cur_username=username)
+
+
 @app.route('/')
 def index():
-    if 'username' in session:
-        app.logger.info('user %s logged in' % session['username'])
-
-        return render_template('index.html', name=session['username'], username=session['username'])
-        # return 'Logged in as %s%s' % (escape(session['username']), (' by %s' % state['agent']) if state['agent'] else '')
     return render_template('index.html')
 
 
@@ -30,8 +42,7 @@ def login():
     if 'username' in session and request.method != 'POST':
         return render_template('login.html',
                                banner_message='User %s has already logged in.' % session['username'],
-                               form=form,
-                               username=session['username'])
+                               form=form)
 
     if request.method == 'POST' and form.validate_on_submit():
         logout()
@@ -45,6 +56,8 @@ def login():
             form.password.errors = [wtforms.ValidationError(u'password does not match')]
         else:
             session['username'] = username
+            app.logger.info('user %s logged in' % session['username'])
+
             if 'Palient/0.0.a' in request.user_agent.string:
                 state['agent'] = 'palient'
             else:
@@ -60,16 +73,24 @@ def login():
 
 @app.route('/user')
 def show_list():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
     user = User.query.filter_by(username=session['username']).first()
     for entity in user.papers + user.dispatches:
         entity.force_status_str()
+
+    forward_form = ForwardForm()
+    forward_form.users_selected.choices = [(u._id, u.username) for u in User.query.all()
+                                           if u._id not in (user._id,
+                                                            User.query.filter_by(username='admin').first()._id)]
 
     return render_template('papers.html',
                            pd_entities_to=sorted(user.papers,
                                                  cmp=lambda x, y: cmp(x.dispatch_date, y.dispatch_date),
                                                  reverse=True),
                            pd_entities_from=user.dispatches,
-                           username=session['username'])
+                           forward_form=forward_form)
 
 
 @app.route('/read', methods=['POST'])
@@ -89,7 +110,20 @@ def read_paper():
 
 @app.route('/forward', methods=['POST'])
 def forward_paper():
-    pass
+    pde = PaperDispatchEntity.query.filter_by(_id=request.form['pde_id']).first()
+
+    for _, user_id in filter(lambda (x, _): x == 'users_selected',
+                             request.form.items()):
+        new_pde = PaperDispatchEntity(request.form['from_uid'],
+                                      user_id,
+                                      pde.paper._id,
+                                      0x1,
+                                      date.today())
+        db.session.add(new_pde)
+
+    db.session.commit()
+
+    return redirect(url_for('show_list'))
 
 
 @app.route('/download', methods=['POST'])
@@ -141,8 +175,6 @@ def logout():
 
     return redirect(url_for('index'))
 
-
-app.secret_key = '''\x8f\xae\x94\xa2\x0b\xcb\x95\xfe\xf9&5\xa4\x9a6\x14\xea5\x84D\xe7'\xcc\x90G'''
 
 
 if __name__ == '__main__':
