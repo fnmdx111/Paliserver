098490b151d099b53728e0ce8fe85fc96eeac6ea
czh <chsc4698@gmail.com>
2012-09-17 02:01:22 +0800

    new MultiCheckboxField introduced

--- palis/forms.py
+++ palis/forms.py
@@ -1,5 +1,7 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, validators, PasswordField
+from wtforms import TextField, validators, PasswordField, SelectMultipleField, widgets
+from palis import app
+from palis.models import User
 
 class LoginForm(Form, object):
     username = TextField(u'Username', validators=[validators.Required()])
@@ -7,4 +9,19 @@ class LoginForm(Form, object):
 
     def __init__(self, form, **kwargs):
         super(LoginForm, self).__init__(form, **kwargs)
+
+
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+
+class ForwardForm(Form, object):
+    users_selected = MultiCheckboxField()
+
+    def __init__(self, **kwargs):
+        super(ForwardForm, self).__init__(**kwargs)
+
+
 
