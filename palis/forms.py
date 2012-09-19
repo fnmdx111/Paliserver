f903669173e00ec8df95cdb08ac3871850890155
czh <chsc4698@gmail.com>
2012-09-20 02:15:20 +0800

    upload introduced and added validation into LoginForm

--- palis/forms.py
+++ palis/forms.py
@@ -1,7 +1,7 @@
-from flask.ext.wtf import Form
+from flask.ext.wtf import Form, FileField
 from wtforms import TextField, validators, PasswordField, SelectMultipleField, widgets
 from palis import app
-from palis.models import User
+from palis.models import User, Paper
 
 class LoginForm(Form, object):
     username = TextField(u'Username', validators=[validators.Required()])
@@ -9,6 +9,24 @@ class LoginForm(Form, object):
 
     def __init__(self, form, **kwargs):
         super(LoginForm, self).__init__(form, **kwargs)
+        self.user = None
+
+
+    def validate(self):
+        request_valid = Form.validate(self)
+        if not request_valid:
+            return False
+
+        user = User.query.filter_by(username=self.username.data).first()
+        if not user:
+            self.username.errors.append(u'user %s not found' % self.username.data)
+            return False
+        if user.password != self.password.data:
+            self.password.errors.append(u'password does not match')
+            return False
+
+        self.user = user
+        return True
 
 
 
@@ -23,5 +41,34 @@ class ForwardForm(Form, object):
     def __init__(self, **kwargs):
         super(ForwardForm, self).__init__(**kwargs)
 
+
+
+class UploadForm(Form, object):
+    title = TextField(u'Title', validators=[validators.Required()])
+    author = TextField(u'Author', validators=[validators.Required()])
+    paper = FileField(u'Paper')
+
+    def __init__(self, **kwargs):
+        super(UploadForm, self).__init__(**kwargs)
+
+
+    def validate(self):
+        request_valid = Form.validate(self)
+        if not request_valid:
+            return False
+
+        if not self.title.data:
+            self.title.errors.append(u'please fill in this field')
+            return False
+        if not self.author.data:
+            self.author.errors.append(u'please fill in this field')
+            return False
+
+        paper = Paper.query.filter_by(author=self.author.data,
+                                      title=self.title.data).first()
+        if paper:
+            return False
+
+        return True
 
 
