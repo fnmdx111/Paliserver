e64d27d246ea3443d41b64f8719f2f04d6d5278e
czh <chsc4698@gmail.com>
2012-09-22 13:52:29 +0800

    changed database name

--- palis/__init__.py
+++ palis/__init__.py
@@ -5,7 +5,7 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///palis.db'
 app.config['SECRET_KEY'] = '''\x8f\xae\x94\xa2\x0b\xcb\x95\xfe\xf9&5\xa4\x9a6\x14\xea5\x84D\xe7'\xcc\x90G'''
 
 app.debug = True
