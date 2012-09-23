e2938680f491a9d4c73623afb820a27ded93b146
czh <chsc4698@gmail.com>
2012-09-23 20:44:47 +0800

    removed app.debug = True for deployment

--- palis/__init__.py
+++ palis/__init__.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///palis.db'
 app.config['SECRET_KEY'] = '''\x8f\xae\x94\xa2\x0b\xcb\x95\xfe\xf9&5\xa4\x9a6\x14\xea5\x84D\xe7'\xcc\x90G'''
 
-app.debug = True
 
 db = SQLAlchemy(app)
 
