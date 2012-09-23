e81c86e9d7e087bcc87a9170bf5cd6f5962c63de
czh <chsc4698@gmail.com>
2012-09-23 20:46:38 +0800

    fixed the problem where everything dynamic returns 404

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.wsgi import WSGIContainer
-from palis import app
+from palis.Paliserver import app # this is important: be sure to run the app object which has url routing in it
 
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(5000)
 IOLoop.instance().start()
+
